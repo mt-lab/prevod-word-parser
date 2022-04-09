14fb1ca115c6fd6a857aa939c48aef73eddf72d1
ku-dmitry <akinc@yandex.ru>
2022-04-10 00:50:21 +0300

    example parser

--- main.py
+++ main.py
@@ -1,2 +1,20 @@
-
+from bs4 import BeautifulSoup
 import requests
+
+
+def request(url):
+    try:
+        return requests.get(url)
+    except requests.exceptions.ConnectionError:
+        pass
+
+
+def get_word(query_word):
+    target_url = "https://en.wiktionary.org/wiki/" + query_word + "#Russian"
+    page_text = requests.get(target_url)
+    parsed_html = BeautifulSoup(page_text.text, features="html.parser")
+    for nominal_noun_form in parsed_html.select('span[class*="Cyrl form-of lang-ru nom|s-form-of origin"]'):
+        return nominal_noun_form.text.strip()
+
+
+print(get_word("сабля"))
